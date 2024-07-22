9174c94fcf1be87c48bfa47cde8bceb2d6a31dc5
delver <dima.plugaturevchik@gmail.com>
2024-07-22 17:26:25 +0300

    removed unused matrices

--- labwork-2/src/Main.java
+++ labwork-2/src/Main.java
@@ -10,43 +10,31 @@ public class Main {
 
     public static void main(String[] args) {
         var matrixGenerator = new MatrixGenerator();
+        var matrix1 = matrixGenerator.generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+        var matrix2 = matrixGenerator.generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
 
-        var matrix1 = new Matrix(new int[][] {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        });
-
-        var matrix2 = new Matrix(new int[][] {
-            {9, 8, 7, 6},
-            {4, 3, 2, 1}
-        });
-
-        var matrix3 = matrixGenerator.generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-        var matrix4 = matrixGenerator.generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-
-        System.out.println("Matrix 3:");
-        matrix3.print2D();
+        System.out.println("Matrix 1:");
+        matrix1.print2D();
         System.out.println();
 
-        System.out.println("Matrix 4:");
-        matrix4.print2D();
+        System.out.println("Matrix 2:");
+        matrix2.print2D();
         System.out.println();
 
         var sequentialCalculator = new SequentialCalculator();
-        var result = sequentialCalculator.multiplyMatrix(matrix3, matrix4);
+        var result = sequentialCalculator.multiplyMatrix(matrix1, matrix2);
         System.out.println("Sequential result:");
         result.print2D();
         System.out.println();
 
-        var foxCalculator = new FoxCalculator(matrix3, matrix4, THREADS_COUNT);
+        var foxCalculator = new FoxCalculator(matrix1, matrix2, THREADS_COUNT);
         var result2 = foxCalculator.multiplyMatrix();
         System.out.println("Fox result:");
         result2.print2D();
         System.out.println();
 
         var ribbonCalculator = new RibbonCalculator();
-        var result3 = ribbonCalculator.multiplyMatrix(matrix3, matrix4, THREADS_COUNT);
+        var result3 = ribbonCalculator.multiplyMatrix(matrix1, matrix2, THREADS_COUNT);
         System.out.println("Ribbon result:");
         result3.print2D();
 
